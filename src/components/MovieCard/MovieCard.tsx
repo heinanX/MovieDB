f7f7a5b893367a9464341ccbdad168091dbe29e4
heinanX <larrii@live.se>
2023-06-04 14:30:27 +0200

    smartified prop

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -6,14 +6,14 @@ interface MovieCardProps {
   movie: Movie
 }
 
-function MovieCard(props: MovieCardProps) {
+function MovieCard({ movie } : MovieCardProps) {
 
   return (
     <div className="MovieCard d-flex">
 {/*       <h1>{props.movie.title}</h1>
       <h1>{ props.movie.id}</h1> */}
       
-        <img src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`} alt="" />
+        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" />
       
       <MarkAsSeenBtn />
     </div>
